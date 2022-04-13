931324c901600b906a648704290154912c93ebfa
ailint <ailinclaratse@gmail.com>
2022-04-13 16:29:10 -0500

    Revert "Revert "change if name == main""
    This reverts commit 657bd2390b84f8b3fc5e0fc9b914d96c8e94ba54.

--- main.py
+++ main.py
@@ -256,4 +256,6 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # For HEROKU
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port, debug=True)
