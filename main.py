01e6dc0608c4e5031ae8cbe486120fef9bbf6b85
ailint <ailinclaratse@gmail.com>
2022-04-16 11:12:20 -0500

    change author.name

--- main.py
+++ main.py
@@ -263,6 +263,7 @@ def edit_post(post_id):
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
+        post.author = edit_form.author.data
         post.img_url = edit_form.img_url.data
         post.body = edit_form.body.data
         db.session.commit()
