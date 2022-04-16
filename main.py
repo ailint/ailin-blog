31fc019d584ebde06a6030c1b77c2cb9ff80a733
ailint <ailinclaratse@gmail.com>
2022-04-16 11:53:13 -0500

    fix author

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def add_new_post():
             subtitle=form.subtitle.data,
             body=form.body.data,
             img_url=form.img_url.data,
-            author=form.author.name.data,
+            author=current_user,
             date=date.today().strftime("%B %d, %Y")
         )
         db.session.add(new_post)
